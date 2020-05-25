77df1d5d5852a26c890d76fb7ebb52b8333813d9
WyeOak <51382526+WyeOak@users.noreply.github.com>
2020-05-25 19:26:44 +0600

    It seems that the logic is correct, but the freemaker failed

--- src/main/java/attractorschool/kz/lesson68/repository/ChatRepository.java
+++
@@ -1,9 +0,0 @@
-package attractorschool.kz.lesson68.repository;
-
-
-
-import attractorschool.kz.lesson68.model.Chat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRepository extends JpaRepository<Chat, Integer> {
-}
