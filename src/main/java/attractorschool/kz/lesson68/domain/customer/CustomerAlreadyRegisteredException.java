a046b7c8903c9249c44e7c5018cc33797e463825
WyeOak <51382526+WyeOak@users.noreply.github.com>
2020-05-25 18:18:24 +0600

    create table users, message&chats

--- src/main/java/attractorschool/kz/lesson68/domain/customer/CustomerAlreadyRegisteredException.java
+++
@@ -1,7 +0,0 @@
-package attractorschool.kz.lesson68.domain.customer;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.CONFLICT)
-public class CustomerAlreadyRegisteredException extends RuntimeException { }
